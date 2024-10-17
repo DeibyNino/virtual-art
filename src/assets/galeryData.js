24da189710db8a6c4a2daac2575cc7270368a5b5
Deibysoyyo <deibysoyyo@hotmail.com>
2024-10-16 20:18:19 -0500

    ajuste pagina de servivios

--- src/assets/galeryData.js
+++ src/assets/galeryData.js
@@ -1,207 +1,99 @@
-// import image1 from ;
-// import image2 from "galery/imagen2.jpeg";
-// import image3 from "galery/imagen3.jpeg";
-// import image4 from "galery/imagen4.jpeg";
-// import image5 from "galery/imagen5.jpeg";
-// import image6 from "galery/imagen6.jpeg";
-// import image7 from "galery/imagen7.jpeg";
-// import image8 from "galery/imagen8.jpeg";
-// import image9 from "galery/imagen9.jpeg";
-// import image10 from "galery/imagen10.jpeg";
-
-// export const galeryData = [
-//   {
-//     id: 1,
-//     image: new URL("./images/galery/imagen1.jpeg", import.meta.url).href,
-//     title: "Image 1",
-//     description: "Image 1 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 2,
-//     image: new URL("./images/galery/imagen2.jpeg", import.meta.url).href,
-//     title: "Image 2",
-//     description: "Image 2 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 3,
-//     image: new URL("./images/galery/imagen3.jpeg", import.meta.url).href,
-//     title: "Image 3",
-//     description: "Image 3 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 4,
-//     image: new URL("./images/galery/imagen4.jpeg", import.meta.url).href,
-//     title: "Image 4",
-//     description: "Image 4 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 5,
-//     image: new URL("./images/galery/imagen5.jpeg", import.meta.url).href,
-//     title: "Image 5",
-//     description: "Image 5 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 6,
-//     image: new URL("./images/galery/imagen6.jpeg", import.meta.url).href,
-//     title: "Image 6",
-//     description: "Image 6 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 7,
-//     image: new URL("./images/galery/imagen7.jpeg", import.meta.url).href,
-//     title: "Image 7",
-//     description: "Image 7 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 8,
-//     image: new URL("./images/galery/imagen8.jpeg", import.meta.url).href,
-//     title: "Image 8",
-//     description: "Image 8 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 9,
-//     image: new URL("./images/galery/imagen9.jpeg", import.meta.url).href,
-//     title: "Image 9",
-//     description: "Image 9 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-//   {
-//     id: 10,
-//     image: new URL("./images/galery/imagen10.jpeg", import.meta.url).href,
-//     title: "Image 10",
-//     description: "Image 10 description",
-//     tecnica: "Oleo sobre lienzo",
-//     size: "100x100 cm",
-//     year: 2024,
-//   },
-// ];
-
-// // import image1 from ;
-// // import image2 from "galery/imagen2.jpeg";
-// // import image3 from "galery/imagen3.jpeg";
-// // import image4 from "galery/imagen4.jpeg";
-// // import image5 from "galery/imagen5.jpeg";
-// // import image6 from "galery/imagen6.jpeg";
-// // import image7 from "galery/imagen7.jpeg";
-// // import image8 from "galery/imagen8.jpeg";
-// // import image9 from "galery/imagen9.jpeg";
-// // import image10 from "galery/imagen10.jpeg";
-
 export const galeryData = [
-  {
-    id: 1,
-    image: "/galery/imagen1.jpeg",
-    title: "Image 1",
-    description: "Image 1 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 2,
-    image: "/galery/imagen2.jpeg",
-    title: "Image 2",
-    description: "Image 2 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 3,
-    image: "/galery/imagen3.jpeg",
-    title: "Image 3",
-    description: "Image 3 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 4,
-    image: "/galery/imagen4.jpeg",
-    title: "Image 4",
-    description: "Image 4 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 5,
-    image: "/galery/imagen5.jpeg",
-    title: "Image 5",
-    description: "Image 5 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 6,
-    image: "/galery/imagen6.jpeg",
-    title: "Image 6",
-    description: "Image 6 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 7,
-    image: "/galery/imagen7.jpeg",
-    title: "Image 7",
-    description: "Image 7 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 8,
-    image: "/galery/imagen8.jpeg",
-    title: "Image 8",
-    description: "Image 8 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 9,
-    image: "/galery/imagen9.jpeg",
-    title: "Image 9",
-    description: "Image 9 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
-  {
-    id: 10,
-    image: "/galery/imagen10.jpeg",
-    title: "Image 10",
-    description: "Image 10 description",
-    tecnica: "Oleo sobre lienzo",
-    size: "100x100 cm",
-    year: 2024,
-  },
+ {
+  id: 1,
+  image: "/galery/imagen1.jpeg",
+  title: "Catedral de Tunja",
+  description: "Vista lateral de la Catedral de la Plaza Bolivar de Tunja",
+  tecnica: "Oleo sobre lienzo",
+  size: "64cm x 79cm",
+  year: 2024,
+ },
+ {
+  id: 2,
+  image: "/galery/imagen2.jpeg",
+  title: "Serenidad otoñal",
+  description:
+   "Un bote solitario espera en un lago de calma y colores vibrantes.",
+  tecnica: "Oleo sobre lienzo",
+  size: "79x52 cm",
+  year: 2024,
+ },
+ {
+  id: 3,
+  image: "/galery/imagen3.jpeg",
+  title: "Árbol dorado",
+  description:
+   "La belleza efímera del otoño reflejada en un paisaje sereno y lleno de vida.",
+  tecnica: "Oleo sobre lienzo",
+  size: "52x65 cm",
+  year: 2024,
+ },
+ {
+  id: 4,
+  image: "/galery/imagen4.jpeg",
+  title: "Melancolía en la plaza",
+  description:
+   "La calma de un pueblo bajo cielos nostálgicos y arquitecturas atemporales.",
+  tecnica: "Oleo sobre lienzo",
+  size: "65x80 cm",
+  year: 2024,
+ },
+ {
+  id: 5,
+  image: "/galery/imagen5.jpeg",
+  title: "Elefante vibrante",
+  description:
+   "la majestuosidad de la naturaleza expresada en un estallido de color y textura",
+  tecnica: "Oleo sobre lienzo",
+  size: "120x80 cm",
+  year: 2024,
+ },
+ {
+  id: 6,
+  image: "/galery/imagen6.jpeg",
+  title: "Vida rural",
+  description:
+   "La paz del campo reflejada en un sendero que conduce a la simplicidad.",
+  tecnica: "Oleo sobre lienzo",
+  size: "58x72 cm",
+  year: 2024,
+ },
+ {
+  id: 7,
+  image: "/galery/imagen7.jpeg",
+  title: "Camino de amor",
+  description:
+   "Un paseo entre flores, donde la naturaleza y la conexión se encuentran",
+  tecnica: "Oleo sobre lienzo",
+  size: "90x106 cm",
+  year: 2024,
+ },
+ {
+  id: 8,
+  image: "/galery/imagen8.jpeg",
+  title: "Quijote y Sancho",
+  description: "El sueño épico de la aventura, contra molinos y desventuras.",
+  tecnica: "Oleo sobre lienzo",
+  size: "65x80 cm",
+  year: 2024,
+ },
+ {
+  id: 9,
+  image: "/galery/imagen9.jpeg",
+  title: "Madre Tierra",
+  description:
+   "La esencia de la vida brotando desde lo profundo de la naturaleza.",
+  tecnica: "Oleo sobre lienzo",
+  size: "48x56 cm",
+  year: 2024,
+ },
+ {
+  id: 10,
+  image: "/galery/imagen10.jpeg",
+  title: "Momento de paz",
+  description: "El aroma del café se mezcla con la frescura de la naturaleza.",
+  tecnica: "Oleo sobre lienzo",
+  size: "100x100 cm",
+  year: 2024,
+ },
 ];
